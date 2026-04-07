d13d619ba07334cccd0c2942af3c0165893700ad
Namoj-design <namojperiakumar@gmail.com>
2026-04-08 01:21:30 +0530

    add testnet configuration

---
+++ hedera-contracts/hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    hashioTestnet: {
+      url: "https://testnet.hashio.io/api",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 296,
+    },
+    hardhat: {}
+  },
+};
+
+export default config;
